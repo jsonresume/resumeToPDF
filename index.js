fb8c0bf6aeafb25f73fb1e8c477169e1806cd9c3
Roland Sharp <rolandnsharp@gmail.com>
2014-06-24 00:00:03 +1000

    working

---
+++ index.js
@@ -0,0 +1,17 @@
+var html5pdf = require("html5-to-pdf");
+var resumeToHtml = require('resume-to-html');
+var fs = require('fs');
+
+function resumeToPDF(resumeData, fileName, callback) {
+
+    resumeToHtml(resumeData, function(htmlResume) {
+
+        fs.writeFileSync('resume.html', htmlResume, 'utf8');
+        html5pdf().from('resume.html').to(process.cwd() + '/' + fileName, function() {
+
+            callback(true);
+        })
+    });
+}
+
+module.exports = resumeToPDF;
