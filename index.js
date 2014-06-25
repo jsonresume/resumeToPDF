395d43f5f020c6aa1e54a47cec022ef931299dea
Roland Sharp <rolandnsharp@gmail.com>
2014-06-25 23:53:04 +1000

    add css comment

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var pdf = require('html-pdf');
 var resumeToHtml = require('resume-to-html');
 
 function resumeToPDF(resumeJson, callback) {
+    // add css into html as:
+    // https://github.com/marcbachmann/node-html-pdf/blob/master/test/businesscard.html
     resumeToHtml(resumeJson, function(htmlResume) {
         pdf.create(htmlResume, {
             // A4 size? what is that?
